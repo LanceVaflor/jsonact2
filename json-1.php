55a76e052a9269e286939970893d0d3310a27d7a
Lance <60122560+LanceVaflor@users.noreply.github.com>
2020-02-01 16:16:26 +0800

    Update json-1.php

--- json-1.php
+++ json-1.php
@@ -2,8 +2,8 @@
 include 'config.php';
 include 'class.Monsters.php';
 header('Content-Type: application/json');
-$Monsters = new Monsters();
-$list=$Monsters->get_Monsters();
+$monsters = new Monsters();
+$list=$monsters->get_Monsters();
 echo "{\"Monsters\":";
 echo json_encode($list);
 echo "}";
